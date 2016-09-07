86b6611d20a29871dc8f9b32fec53f5479239a78
Argen Gian I Detoito <ad100@pvhs-lab112-d07.pvsd.org>
2016-09-07 14:20:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+        System.out.print("What is going on?");
+        // Didn't replace completely.
+        // So the quotes are still in the same place.
+        // I typed this as a line comment.
+
+        /* This is a
+        block comment.
+         */
+
+        System.out.println(1);
+        System.out.println(1.0);
+
+    }
+}
